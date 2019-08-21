bfc9f6f552e40e6b69fb6c36049a6bdc6469f3a1
Leonardo <lesoria96@gmail.com>
2019-08-21 17:54:14 -0500

    Adding script tag

---
+++ Js/index.js
@@ -0,0 +1,50 @@
+var text = ["atención 24/7", "generación de reportes", "comparación de pedidos"]
+var i = 0;
+var txt = 'FUNCIONA! NOMAMES'
+var line = document.getElementById('showBenefits').innerHTML
+
+function lel() {
+  const element = text[i];
+  if (i < element.length) {
+    document.getElementById('showBenefits').innerHTML += text[0].charAt(i);
+    i++;
+    console.log(line);
+    setTimeout(lel, 100);
+  }
+  for (let i = 0; i < element.length; i++) {
+    // const element = element[i];
+    document.getElementById('showBenefits').innerHTML += element.charAt(i)
+    // console.log(element)
+  }
+}
+function ahoraSi() {
+  document.getElementById('showBenefits').innerHTML = "";
+  setTimeout(() => {
+    writeLog();
+  }, 1000)
+}
+
+function ahoraNo() {
+  document.getElementById('showBenefits').innerHTML += text[0]
+  deleteText();
+}
+function deleteText() {
+  large = text[0].length
+  large = i;
+  if (i >= large) {
+    document.getElementById('showBenefits').innerHtml -= text[0].charAt(large)
+    large--;
+    setTimeout(deleteText, 90);
+  }
+}
+
+function writeLog() {
+  if (i < text[0].length) {
+    document.getElementById('showBenefits').innerHTML += text[0].charAt(i);
+    i++;
+    console.log(line);
+    setTimeout(writeLog, 90);
+  }
+}
+
+// window.onload = writeBenefits()
